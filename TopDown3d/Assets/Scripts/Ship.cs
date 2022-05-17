6c3cead96c3cdc61475d8d288ef91515873efdd2
TestSoul <s.ekker@zmeke.com>
2022-05-17 17:05:50 +0300

    ///

--- TopDown3d/Assets/Scripts/Ship.cs
+++ TopDown3d/Assets/Scripts/Ship.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour
 {
-    public float speed = 1f;
+    public float speed = 5f;
     public Joystick joystick;
     private float vertical;
     private float horizontal;
-    bool rotateObjectBoolr;
-    bool rotateObjectBooll;
+    //bool rotateObjectBoolr;
+    //bool rotateObjectBooll;
     public float rotationspeed = 100f;
 
 
@@ -23,21 +23,26 @@ public class Ship : MonoBehaviour
         GetKeyInput();
         GetMobileInput();
 
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            StartCoroutine(RotateObjectl());
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            StartCoroutine(RotateObjectr());
-        }
+        //Делал поворот вместе с движением
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    StartCoroutine(RotateObjectl());
+        //}
         //else
         //{
-        //    StartCoroutine(RotateObjectdef());
+        //    if (Input.GetKeyDown(KeyCode.D))
+        //    {
+        //        StartCoroutine(RotateObjectr());
+        //    }
+        //    else
+        //    {
+        //        StartCoroutine(RotateObjectdef());
+        //    }
         //}
+
     }
 
-
+    // Управление с пк
     private void GetKeyInput()
     {
         float move = Input.GetAxis("Horizontal");
@@ -47,50 +52,53 @@ public class Ship : MonoBehaviour
         transform.position += new Vector3(0, move1, 0) * speed * Time.deltaTime * 2;
     }
 
-    IEnumerator RotateObjectr()
-    {
-        float moveSpeed = 1000f;
-        Quaternion endingAngle = Quaternion.Euler(new Vector3(-90, -25, 0));
-        //while (Vector3.Distance(transform.rotation.eulerAngles, endingAngle.eulerAngles) > 0.05f)
-        //{
-        //    transform.rotation = Quaternion.RotateTowards(transform.rotation, endingAngle, moveSpeed * Time.deltaTime);
-        //    yield return null;
-        //}
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, endingAngle, moveSpeed * Time.deltaTime);
-        yield return null;
-        transform.rotation = endingAngle;
-    }
-    IEnumerator RotateObjectl()
-    {
-        float moveSpeed = 1000f;
-        Quaternion endingAngle = Quaternion.Euler(new Vector3(-90, 25, 0));
-        //while (Vector3.Distance(transform.rotation.eulerAngles, endingAngle.eulerAngles) > 0.05f)
-        //{
-        //    transform.rotation = Quaternion.RotateTowards(transform.rotation, endingAngle, moveSpeed * Time.deltaTime);
-        //    yield return null;
-        //}
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, endingAngle, moveSpeed * Time.deltaTime);
-        yield return null;
-        transform.rotation = endingAngle;
-    }
-    IEnumerator RotateObjectdef()
-    {
-        float moveSpeed = 1000f;
-        Quaternion endingAngle = Quaternion.Euler(new Vector3(-90, 0, 0));
-        while (Vector3.Distance(transform.rotation.eulerAngles, endingAngle.eulerAngles) > 0.05f)
-        {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, endingAngle, moveSpeed * Time.deltaTime);
-            yield return null;
-        }
-        transform.rotation = endingAngle;
-    }
+    // Поворот вместе с движением
+    //IEnumerator RotateObjectr()
+    //{
+    //    float moveSpeed = 1000f;
+    //    Quaternion endingAngle = Quaternion.Euler(new Vector3(-90, -25, 0));
+    //    //while (Vector3.Distance(transform.rotation.eulerAngles, endingAngle.eulerAngles) > 0.05f)
+    //    //{
+    //    //    transform.rotation = Quaternion.RotateTowards(transform.rotation, endingAngle, moveSpeed * Time.deltaTime);
+    //    //    yield return null;
+    //    //}
+    //    transform.rotation = Quaternion.RotateTowards(transform.rotation, endingAngle, moveSpeed * Time.deltaTime);
+    //    yield return null;
+    //    transform.rotation = endingAngle;
+    //}
+    //IEnumerator RotateObjectl()
+    //{
+    //    float moveSpeed = 1000f;
+    //    Quaternion endingAngle = Quaternion.Euler(new Vector3(-90, 25, 0));
+    //    //while (Vector3.Distance(transform.rotation.eulerAngles, endingAngle.eulerAngles) > 0.05f)
+    //    //{
+    //    //    transform.rotation = Quaternion.RotateTowards(transform.rotation, endingAngle, moveSpeed * Time.deltaTime);
+    //    //    yield return null;
+    //    //}
+    //    transform.rotation = Quaternion.RotateTowards(transform.rotation, endingAngle, moveSpeed * Time.deltaTime);
+    //    yield return null;
+    //    transform.rotation = endingAngle;
+    //}
+    //IEnumerator RotateObjectdef()
+    //{
+    //    float moveSpeed = 1000f;
+    //    Quaternion endingAngle = Quaternion.Euler(new Vector3(-90, 0, 0));
+    //    while (Vector3.Distance(transform.rotation.eulerAngles, endingAngle.eulerAngles) > 0.05f)
+    //    {
+    //        transform.rotation = Quaternion.RotateTowards(transform.rotation, endingAngle, moveSpeed * Time.deltaTime);
+    //        yield return null;
+    //    }
+    //    transform.rotation = endingAngle;
+    //}
+
+    // Управление с мобилки
 
     private void GetMobileInput()
     {
         vertical = joystick.Vertical;
         horizontal = joystick.Horizontal;
 
-
+        //Движение вверх
         if (vertical >= 0.1)
         {
             if (vertical >= 0.5)
@@ -102,6 +110,7 @@ public class Ship : MonoBehaviour
                 transform.position += new Vector3(0, 1, 0) * speed * Time.deltaTime;
             }
         }
+        //Движение вниз
         if (vertical <= -0.1)
         {
             if (vertical <= -0.5)
@@ -113,6 +122,7 @@ public class Ship : MonoBehaviour
                 transform.position += new Vector3(0, -1, 0) * speed * Time.deltaTime;
             }
         }
+        //Движение влево
         if (horizontal <= -0.1)
         {
             if (horizontal <= -0.5)
@@ -124,6 +134,7 @@ public class Ship : MonoBehaviour
                 transform.position += new Vector3(-1, 0, 0) * speed * Time.deltaTime;
             }
         }
+        //Движение вправо
         if (horizontal >= 0.1)
         {
             if (horizontal >= 0.5)
